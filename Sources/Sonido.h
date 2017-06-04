c5474f58e072297bd6576caeaf889ad53ae48a83
ivankras <ivankras@hotmail.com>
2017-06-03 23:45:54 -0300

    Merge con iv3, error en mcuinit

--- Sources/Sonido.h
+++ Sources/Sonido.h
@@ -10,7 +10,7 @@
 
 void SONIDO_init(void);
 void setFrecuenciaActual(unsigned int fa);
-unsigned int SONIDO_prender_m1(void); /* Enciende el sonido en una frecuencia fija(se asume seteada previamente); devuelve el error (Hz) existente entre la frecuencia del sonido real respecto del deseado. */
+void SONIDO_prender_m1(void); /* Enciende el sonido en una frecuencia fija(se asume seteada previamente); devuelve el error (Hz) existente entre la frecuencia del sonido real respecto del deseado. */
 void SONIDO_prender_m2(unsigned char T); /* Dispara el barrido de frecuencias, para que se realice en T segundos */
 void SONIDO_apagar(void);
 void SONIDO_reanudar(void);
